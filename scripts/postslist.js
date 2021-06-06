acd3a3cf532f44063635031a28fa7c5508431f43
Bhargava Raju <BhargavaR@anthology.com>
2021-06-06 19:57:48 +0530

    Adding the delete confimation button logic

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,28 +1,41 @@
 var deleteBtn = document.getElementsByClassName('deleteBlog');
+var selectedPostId = 0;
 
 var popUpAlter = document.getElementById('modalPopUp');
 
 deleteBtn[0].onclick = function(){
     popUpAlter.style.display="block";
+    selectedPostId = 0;
 }
 
 deleteBtn[1].onclick = function(){
     popUpAlter.style.display="block";
+    selectedPostId = 1;
 }
 
 deleteBtn[2].onclick = function(){
     popUpAlter.style.display="block";
+    selectedPostId = 2;
 }
 
 deleteBtn[3].onclick = function(){
     popUpAlter.style.display="block";
+    selectedPostId = 3;
 }
 
 deleteBtn[4].onclick = function(){
     popUpAlter.style.display="block";
+    selectedPostId = 4;
 }
 var closeBtn = document.getElementById('noClose');
 
 closeBtn.onclick= function(){
+    popUpAlter.style.display="none";
+}
+
+var deleteConfirmBtn = document.getElementById('yesDelete');
+deleteConfirmBtn.onclick= function(){
+    var selectedPost = document.getElementsByClassName('post');
+    selectedPost[selectedPostId].style.display = "none";
     popUpAlter.style.display="none";
 }
