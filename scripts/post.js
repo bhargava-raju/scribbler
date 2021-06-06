a88422e1a4ccafe2b70c6cbd025c3e8564a5c9a2
Bhargava Raju <BhargavaR@anthology.com>
2021-06-06 23:20:36 +0530

    Adding markup and style for comments grid and minor changes

--- scripts/post.js
+++ scripts/post.js
@@ -3,37 +3,47 @@ var contentText = document.getElementById('postBody');
 var editButton = document.getElementById('editButton');
 var saveButton = document.getElementById('saveButton');
 // edit and save the content
-function editContent(){
-    contentText.contentEditable="true";
-    titleConetnt.contentEditable="true";
-    contentText.style.border="1px solid red";
-    titleConetnt.style.border="1px solid red";
-    editButton.style.display="none";
-    saveButton.style.display="block";
+function editContent() {
+    contentText.contentEditable = "true";
+    titleConetnt.contentEditable = "true";
+    contentText.style.border = "1px solid red";
+    titleConetnt.style.border = "1px solid red";
+    editButton.style.display = "none";
+    saveButton.style.display = "block";
     contentText.focus();
 }
 
-function saveContent(){
-    contentText.contentEditable="false";
-    titleConetnt.contentEditable="false";
-    contentText.style.border="none";
-    titleConetnt.style.border="none";
-    editButton.style.display="block";
-    saveButton.style.display="none"; 
+function saveContent() {
+    contentText.contentEditable = "false";
+    titleConetnt.contentEditable = "false";
+    contentText.style.border = "none";
+    titleConetnt.style.border = "none";
+    editButton.style.display = "block";
+    saveButton.style.display = "none";
 }
 
-var numberOfClicks=0;
-function postLiked(){
-   var button = document.getElementById("addLikes")
-   button.innerHTML = '<i class="fa fa-thumbs-up ">'+'</i>'+'&nbsp;'+"Liked!";
-   button.style.border="none";
+var numberOfClicks = 0;
+function postLiked() {
+    var button = document.getElementById("addLikes")
+    button.innerHTML = '<i class="fa fa-thumbs-up ">' + '</i>' + '&nbsp;' + "Liked!";
+    button.style.border = "none";
     numberOfClicks += 1;
-    if(numberOfClicks!=0){
-        if (numberOfClicks==1){
+    if (numberOfClicks != 0) {
+        if (numberOfClicks == 1) {
             document.getElementById('likeCount').innerHTML = numberOfClicks + " person likes this!";
         }
         else {
             document.getElementById('likeCount').innerHTML = numberOfClicks + " people have liked this!";
         }
-        }
     }
+}
+
+//comments function..
+function addComment(id) {
+    if (id.value.length > 0) {
+        var addEventName = id.value;
+        document.getElementById('addComment').innerHTML = '<p class="comment">' + addEventName + '</p>' + document.getElementById('addComment').innerHTML;
+        var a = document.getElementById('userComments');
+        a.value = a.defaultValue;
+    }
+}
